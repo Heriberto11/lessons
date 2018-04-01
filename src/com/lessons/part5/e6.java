65a20a94f8c68fdd5efb10f17f34ebb3e9c1a240
Heriberto Amastal <betoamastal@gmail.com>
2018-03-31 22:53:18 -0400

    Initial commit. All exercises so far.

--- src/com/lessons/part5/e6.java
+++ src/com/lessons/part5/e6.java
@@ -3,10 +3,9 @@ package com.lessons.part5;
 public class e6 {
     public static void main(String[] args) {
         //6)	Dado un número entero, mostrar todos los múltiplos de 5 comprendidos entre 1 y el número leído.
-        int number = 35;
+        int number = 38;
 
-        for (int i = 1; i <= number; i++)
-            if(i%5==0)
+        for (int i = 0; i <= number; i+=5)
                 System.out.println(i);
     }
 }
